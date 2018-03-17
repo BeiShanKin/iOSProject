7ceeba8ae44c60645f650fd8edf4727c13e23e49
songyu <syiosengineer1992@gmail.com>
2018-03-17 14:33:59 +0800

    初始化

---
+++ Objective_C/Objective_C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Objective_C
+//
+//  Created by soy on 2018/3/17.
+//  Copyright © 2018年 soy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objective_C/Objective_C/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Objective_C
+//
+//  Created by soy on 2018/3/17.
+//  Copyright © 2018年 soy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
